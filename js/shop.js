dfa4296319ed05cdda1143bfdf6d97c3971ed1ae
Ivan <64588529+ivanymc@users.noreply.github.com>
2022-06-14 23:38:54 +0800

    update shopping cart

--- js/shop.js
+++ js/shop.js
@@ -4,7 +4,7 @@
 const SHOPPING_CART_BUTTON = document.querySelector(".shopping-cart-button");
 const SHOPPING_CART_NUMBER = document.querySelector(".shopping-cart-number");
 
-SHOPPING_CART_BUTTON.addEventListener('click', e => {
+function displayShoppingCart(e) {
     let shoppingCart = e.target.parentNode.parentNode.parentNode.parentNode.nextElementSibling;
 
     if (shoppingCart.style.display === "none") {
@@ -12,17 +12,10 @@ SHOPPING_CART_BUTTON.addEventListener('click', e => {
     } else {
         shoppingCart.style.display = "none";
     } 
-})
+}
 
-SHOPPING_CART_NUMBER.addEventListener('click', e => {
-    let shoppingNumber = e.target.parentNode.parentNode.parentNode.parentNode.nextElementSibling;
-
-    if (shoppingNumber.style.display === "none") {
-        shoppingNumber.style.display = "flex"; // must be D-flex to align items 
-    } else {
-        shoppingNumber.style.display = "none";
-    } 
-})
+SHOPPING_CART_BUTTON.addEventListener('click', displayShoppingCart)
+SHOPPING_CART_NUMBER.addEventListener('click', displayShoppingCart)
 
 
 /*
@@ -30,6 +23,8 @@ SHOPPING_CART_NUMBER.addEventListener('click', e => {
 */
 const ADD_TO_SHOPPING_CART_BUTTON = document.querySelectorAll('.add-to-shopping-cart');
 const SHOPPING_CART_LIST = document.querySelector('.shopping-cart-uppergrid');
+const SHOPPING_CART = document.querySelector('.shopping-cart');
+
 
 // QuerySelectorAll is nodelist
 Array.from(ADD_TO_SHOPPING_CART_BUTTON).forEach( button => {
@@ -42,8 +37,12 @@ Array.from(ADD_TO_SHOPPING_CART_BUTTON).forEach( button => {
         // .outerHTML directly convert to string
         let text = productName.outerHTML + productImage.outerHTML + productPrice.outerHTML 
         
-        SHOPPING_CART_LIST.insertAdjacentHTML("afterbegin", text);  
+        SHOPPING_CART_LIST.insertAdjacentHTML("afterbegin", text);
+        if (SHOPPING_CART.style.display === "none") {
+            SHOPPING_CART.style.display = "flex";
+        }  
         button.disabled = true; // avoid multi click
+
     })
 });
 
