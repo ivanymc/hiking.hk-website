2d9bb455e8b5e3033fd8fb5cc8580bf8763d56f8
Evanc-1010 <107043407+Evanc-1010@users.noreply.github.com>
2022-06-15 18:47:04 +0800

    Added about.html, style_about.css, about.js
    Mainly wrote text paragraph, font size adjustment.

---
+++ about.js
@@ -0,0 +1,23 @@
+let textFont = document.getElementById("textHeadContainer");
+let fontRange = document.getElementById("fontRange");
+
+
+function changeFontSizeSm() {
+    textFont.style.fontSize = "14px";
+    fontRange.value = 14;
+};
+
+function changeFontSizeMd() {
+    textFont.style.fontSize = "17px";
+    fontRange.value = 17;
+};
+
+function changeFontSizeLg() {
+    textFont.style.fontSize = "26px";
+    fontRange.value = 26;
+};
+
+function changeFontSizeRange() {
+    textFont.style.fontSize = fontRange.value+"px";
+    
+};
